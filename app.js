b50403db1b5af014dfcce0ac037385d026c61f83
chongsoon <chongsoon@ntu.edu.sg>
2018-02-19 15:10:19 +0800

    add gitignore and pug files, updated package json

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ var app = express(),
 path = require('path'),
 publicDir = path.join(__dirname,'public');
 
-app.use(express.static(publicDir))
+//app.use(express.static(publicDir))
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.get('/', function (req, res) {
+    res.render('index', { title: 'Hey', message: 'Hello there!' });
+});
 
 app.listen(port);
